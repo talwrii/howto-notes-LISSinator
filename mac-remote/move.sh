20523d4685d2f211480d0fbcaadfd6ed20131d5a
Alex <alex@cexs-MacBook-Pro.local>
2023-06-27 01:13:11 +0100

    move commands

---
+++ mac-remote/move.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+hs <<EOF
+spaces = require("hs.spaces")
+-- move current window to the space sp
+function MoveWindowToSpace(sp)
+  local win = hs.window.focusedWindow()      -- current window
+  local cur_screen = hs.screen.mainScreen()
+  local cur_screen_id = cur_screen:getUUID()
+  local all_spaces=spaces.allSpaces()
+  local spaceID = all_spaces[cur_screen_id][sp]
+  spaces.gotoSpace(spaceID)              -- follow window to new space
+end
+MoveWindowToSpace($1)
+EOF
+
---
+++ mac-remote/readme.md
@@ -0,0 +1,10 @@
+Requires hammerspoon and karabiner
+
+move.sh 1 # moves to the first desktop
+
+#Hacking
+
+Open the following uri in to load config in karabiner
+```
+karabiner://karabiner/assets/complex_modifications/import?url=file:///Users/alex/LISSinator/mac-remote/remote.karabiner.json
+```
